a046af66b8189c9a271b3bd1061ec785b9d361fa
Meri <meri_sk991@hotmail.com>
2018-04-02 13:07:34 +0200

    images

--- ExpresionsComponents.js
+++ ExpresionsComponents.js
@@ -7,5 +7,14 @@
   DICtrl.$inject = ['$scope'];
   function DICtrl($scope) {
     $scope.name = "Meri";
+    $scope.stateOfBeing = "hungry";
 
+    $scope.sayMessage = function () {
+      return "MessageMessageMessageMessageMessageMessageMessageMessage!!!"
+    };
+
+    $scope.feed = function () {
+      $scope.stateOfBeing = "fed";
+    };
+};
 })();
